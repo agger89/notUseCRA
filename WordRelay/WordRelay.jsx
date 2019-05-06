a0aa9895882b95417aedb939c8e4c1609de6962d
agger89 <agger2154@gmail.com>
2019-05-06 16:36:51 +0900

    구구단 기능 개발 완료

--- WordRelay/WordRelay.jsx
+++ WordRelay/WordRelay.jsx
@@ -1,13 +1,51 @@
 const React = require('react');
 const { Component } = React;
 
-class WordRelay extends React.Component {
+class WordRelay extends Component {
     state = {
-        text: 'Hello, webpack'
+        word: '조니',
+        value: '',
+        result: '',
     };
 
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if(this.state.word[this.state.word.length -1] === this.state.value[0]) {
+            this.setState({
+                result: '딩동댕',
+                word: this.state.value,
+                value: ''
+            });
+            this.input.focus();
+        } else {
+            this.setState({
+                result: '땡',
+                value: ''
+            });
+            this.input.focus();
+        }
+    }
+
+    onChangeInput = (e) => {
+        this.setState({ value: e.target.value });
+    }
+
+    input;
+    onRefInput = (c) => {
+        this.input = c;
+    }
+
     render() {
-        return <h1>{this.state.text}</h1>
+        return (
+            <>
+                <div>{this.state.word}</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+                    <button>입력!</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        )
     }
 }
 
